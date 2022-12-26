d9d12ef7756c8aba687ebb7197029ba154fde594
Esolder <40249820+Esolder@users.noreply.github.com>
2022-12-26 09:44:40 +0300

    исправил баг

--- publish_one_photo.py
+++ publish_one_photo.py
@@ -3,14 +3,18 @@ import os
 import random
 
 import telegram
-
+from dotenv import load_dotenv
 from support_funcs import get_files_paths
 
 
 def publish_in_telegram(photo_name, folderpath, telegram_bot, chat_id):
     if photo_name is None:
         photo_names = get_files_paths(folderpath)
-        photo_name = random.choice(photo_names)
+        if photo_names:
+            photo_name = random.choice(photo_names)
+        else:
+            print('Отсутствуют фотографии в дериктории')
+            return
 
     with open(photo_name, 'rb') as ph:
         telegram_bot.send_photo(photo=ph, chat_id=chat_id)
@@ -22,6 +26,7 @@ if __name__ == '__main__':
     parser.add_argument('--photo', help='Имя фотографии')
     args = parser.parse_args()
 
+    load_dotenv()
     folderpath = os.getenv('FOLDERPATH', 'images')
     bot = telegram.Bot(token=os.environ['TELEGRAM_TOKEN'])
     chat_id = os.environ['TELEGRAM_CHAT_ID']
